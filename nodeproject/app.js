731db056eb6fc5a539914b7598102ea02aa96c12
Farid <farid.rahimov@yahoo.com>
2017-02-26 10:08:49 +0000

    Port number

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,6 @@
-var Alohe= "Aloha variables"
-console.log(Alohe)
+var express = require("express");
+var app = express();
+var port = process.env.PORT;
+app.listen(port, function(err){
+    console.log("Port number is: "+ port)
+})
