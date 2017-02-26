950cdb9c2bca027687e7c1f5912ce7ecb09efaf0
Farid <farid.rahimov@yahoo.com>
2017-02-26 14:08:32 +0000

    routinh

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT;
+
+app.get("/",function(req,res){
+    res.send("Hello World")
+})
+
+app.get("/routing",function(req,res){
+    res.send("Hello Routing")
+})
+
 app.listen(port, function(err){
     console.log("Port number is: "+ port)
 })
